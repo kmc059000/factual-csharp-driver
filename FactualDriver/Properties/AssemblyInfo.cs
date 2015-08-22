aa15653ee25979c7710a8ae6f35fa3890f6291b7
Kenneth Crawford <kcrawford@aprima.com>
2015-08-22 11:02:13 -0500

    project issues

--- FactualDriver/Properties/AssemblyInfo.cs
+++ FactualDriver/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("15f4fd57-7beb-4a95-bcb0-9982c38d0036")]
+//[assembly: Guid("15f4fd57-7beb-4a95-bcb0-9982c38d0036")]
 
 // Version information for an assembly consists of the following four values:
 //
